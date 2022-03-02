4d12b4dc188beadb89f1ae94d657518fc6b83b97
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-02 11:36:06 +0800

    Eloquent Updates and HTML Escaping

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,9 +7,9 @@
 <x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
-        <p>
+        <div>
             {!! $post->body !!}
-        </p>
+        </div>
     </article>
     <a href="/">Go Back</a>
 </x-layout>
