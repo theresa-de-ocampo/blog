c6a8ed7c73073034d0bee23ba13643c477578538
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-08 22:25:09 +0800

    [36] Quick Tweaks and Clean-Up

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -49,7 +49,7 @@
                 </h1>
 
                 <div class="space-y-4 lg:text-lg leading-loose">
-                    <p>{{ $post->body }}</p>
+                    <p>{!! $post->body !!}</p>
                 </div>
             </div>
         </article>
