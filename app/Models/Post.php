9293e8863d68d8cd28adeb51408086b35decac6c
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-10 17:46:42 +0800

    [38] Search (Cleaner Way)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,20 @@ class Post extends Model
     protected $guarded = []; // Never perform mass assignment unless you're in control of the array.
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, array $filters) { // Post::newQuery()->filter()
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')
+        );
+
+        /*if ($filters['search'] ?? false) {
+            $query
+                ->where('title', 'like', '%' . $filters['search'] . '%')
+                ->orWhere('body', 'like', '%' . $filters['search'] . '%');
+        }*/
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
