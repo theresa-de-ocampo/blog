0f3898ab3e21164ec18740485e4bd67ff263d525
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-11 15:30:03 +0800

    [42] Merge Category & Search Queries

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -12,7 +12,7 @@
 
     @foreach ($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
             :active="$category->is($currentCategory)"
         >
             {{ ucwords($category->name) }}
