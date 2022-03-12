10ff9311c224a11a0d0a8922faaec827d88a7d06
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-12 22:06:51 +0800

    [50] Build the Log In Page

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,6 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
-Route::post('logout', [SessionsController::class, 'destroy']);
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('sessions', [SessionsController::class, 'store']);
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
