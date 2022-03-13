7e5e4d247122e2883dbc3e694b137a2f60437931
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-13 15:43:42 +0800

    [52] Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,15 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" class="rounded-xl">
+    </div>
+
+    <div>
+        <header mb-4>
+            <h3 class="font-bold">Selena Gomez</h3>
+            <p class="text-xs">Posted <time>8 months ago</time></p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, assumenda eius ipsam maiores quo ratione sit unde vero. Architecto illo labore perspiciatis porro possimus quos sapiente sint totam ullam voluptate.
+        </p>
+    </div>
+</article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -56,6 +56,12 @@
                     <p>{!! $post->body !!}</p>
                 </div>
             </div>
+
+            <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
+            </section>
         </article>
     </main>
 </x-layout>
