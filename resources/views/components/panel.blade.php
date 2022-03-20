d331d84cd505672a6c562f798412a2cd2ae02873
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-20 20:15:28 +0800

    [55] Design the Comment Form

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes(['class' => 'p-6 rounded-xl border border-gray-200']) }}>
+    {{ $slot }}
+</div>
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,17 +1,21 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100?u={{ $comment->author->id }}" alt="" class="rounded-xl">
-    </div>
+<x-panel class="bg-gray-50 border border-gray-200">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/100?u={{ $comment->author->id }}" alt="" class="rounded-xl">
+        </div>
 
-    <div>
-        <header mb-4>
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
-            <p class="text-xs">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></p>
-        </header>
-        <p>
-            {{ $comment->body }}
-        </p>
-    </div>
-</article>
+        <div>
+            <header mb-4>
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">Posted
+                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
+            <p>
+                {{ $comment->body }}
+            </p>
+        </div>
+    </article>
+</x-panel>
