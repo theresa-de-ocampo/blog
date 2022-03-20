b6f9059127cf1b1d82ebdd74661396b1373c4081
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-20 21:33:53 +0800

    [57] Some Light Chapter Clean Up

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -58,43 +58,7 @@
             </div>
 
             <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                @auth
-                    <x-panel>
-                        <form method="POST" action="/posts/{{ $post->slug }}/comments">
-                            @csrf
-
-                            <header class="flex items-center">
-                                <img
-                                    src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt=""
-                                    width="50" height="50" class="rounded-full">
-                                <h2 class="ml-4">Want to participate?</h2>
-                            </header>
-
-                            <div class="mt-6">
-                            <textarea
-                                name="body" rows="5"
-                                class="w-full focus:outline-none focus:ring"
-                                placeholder="You may use mark down with GitHub-flavored code blocks."></textarea>
-                            </div>
-
-                            <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
-                                <button
-                                    type="submit"
-                                    class="bg-blue-500 text-white uppercase font-semibold text-xs
-                                    py-2 px-10 rounded-2xl hover:bg-blue-600"
-                                >
-                                    Post
-                                </button>
-                            </div>
-                        </form>
-                    </x-panel>
-                @else
-                    <p class="font-semibold">
-                        <a href="/register" class="hover:underline">Register</a> or
-                        <a href="/login" class="hover:underline">log in</a> to post a comment.
-                    </p>
-                @endauth
-
+                @include('posts._add-comment-form')
                 @foreach ($post->comments as $comment)
                     <x-post-comment :comment="$comment" />
                 @endforeach
