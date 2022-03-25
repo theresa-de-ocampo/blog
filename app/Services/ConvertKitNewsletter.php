29964995bc349ef3b9e3225053afcc180fe95984
theresa-de-ocampo <ma_theresa7@yahoo.com>
2022-03-25 16:00:27 +0800

    [61] Toy Chests and Contracts

---
+++ app/Services/ConvertKitNewsletter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class ConvertKitNewsletter implements Newsletter
+{
+    public function subscribe(string $email, string $list = null)
+    {
+        // TODO: Implement subscribe() method.
+    }
+}
